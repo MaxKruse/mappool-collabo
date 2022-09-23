548bf8e97473d97a84d72c9c3561e3d77e55c53b
Maximilian Kruse <maxkruse1996@gmail.com>
2022-09-23 23:44:27 +0200

    implement update tournament (put)

--- backend/routes/tournament/tournament.go
+++ backend/routes/tournament/tournament.go
@@ -15,7 +15,10 @@ func List(c *fiber.Ctx) error {
 
 func Get(c *fiber.Ctx) error {
 	id := c.Params("id")
-	tournament := tournamentservice.GetTournament(id)
+	tournament, err := tournamentservice.GetTournament(id)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Not found"})
+	}
 	return c.JSON(tournament)
 }
 
@@ -23,13 +26,13 @@ func Create(c *fiber.Ctx) error {
 	// get self from context
 	self, err := userservice.GetUserFromToken(c.Get("Authorization")[7:])
 	if err != nil {
-		return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 	}
 
 	// get tournament from request
 	var tournament models.TournamentDto
 	if err := c.BodyParser(&tournament); err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "Bad request"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Bad request"})
 	}
 
 	// set owner to self
@@ -38,16 +41,70 @@ func Create(c *fiber.Ctx) error {
 	// create tournament
 	newTournament, err := tournamentservice.CreateTournament(tournament)
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "Bad request"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Bad request"})
 	}
 
 	return c.JSON(newTournament)
 }
 
 func Update(c *fiber.Ctx) error {
-	return nil
+	// get self from context
+	self, err := userservice.GetUserFromToken(c.Get("Authorization")[7:])
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
+	}
+
+	// get tournament from path param
+	id := c.Params("id")
+	tournament, err := tournamentservice.GetTournament(id)
+
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Not found"})
+	}
+
+	// check if self is owner
+	if tournament.Owner.ID != self.ID {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "Forbidden"})
+	}
+
+	// get tournament from request
+	var tournamentDto models.TournamentDto
+	if err := c.BodyParser(&tournamentDto); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Bad request"})
+	}
+
+	tournamentDto.ID = tournament.ID
+
+	// update tournament
+	updatedTournament, err := tournamentservice.UpdateTournament(tournamentDto)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
+	}
+
+	return c.JSON(updatedTournament)
 }
 
 func Delete(c *fiber.Ctx) error {
-	return nil
+	// get self from context
+	self, err := userservice.GetUserFromToken(c.Get("Authorization")[7:])
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
+	}
+
+	// get tournament from path param
+	id := c.Params("id")
+	tournament, err := tournamentservice.GetTournament(id)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Not found"})
+	}
+
+	// check if self is owner
+	if tournament.Owner.ID != self.ID {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "Forbidden"})
+	}
+
+	// delete tournament
+	tournamentservice.DeleteTournament(id)
+
+	return c.SendStatus(fiber.StatusNoContent)
 }
