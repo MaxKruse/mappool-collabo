28fb871aab302bade40f10e35ad671d70d5d73e7
Maximilian Kruse <maxkruse1996@gmail.com>
2022-09-24 12:42:02 +0200

    fix eslint vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    eslint()
+    vue()
   ],
   server: {
     port: 8080,
