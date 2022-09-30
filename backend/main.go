6a892fdf6a1f9e86a1b8c7f44ea2bea2a10866c0
Maximilian Kruse <maxkruse1996@gmail.com>
2022-09-30 23:02:00 +0200

    Refactor route adding

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"backend/routes/oauth"
-	"backend/routes/tournament"
-	"backend/routes/user"
+	"backend/routes"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
@@ -28,32 +26,7 @@ func main() {
 	// make session
 
 	// Our groups
-	usersGroup := app.Group("/users")
-	{
-		usersGroup.Get("/", user.List)
-		usersGroup.Get("/self", user.GetSelf)
-		usersGroup.Get("/:id", user.Get)
-	}
-
-	oauthGroup := app.Group("/oauth")
-	{
-		oauthGroup.Get("/login", oauth.Login)
-	}
-
-	tournamentGroup := app.Group("/tournaments")
-	{
-		tournamentGroup.Get("/", tournament.List)
-		tournamentGroup.Get("/:id", tournament.Get)
-		tournamentGroup.Post("/", tournament.Create)
-		tournamentGroup.Put("/:id", tournament.Update)
-		tournamentGroup.Delete("/:id", tournament.Delete)
-	}
-
-	mappoolerGroup := app.Group("/mappoolers")
-	{
-		mappoolerGroup.Post("/", tournament.AddMappooler)
-		mappoolerGroup.Delete("/", tournament.RemoveMappooler)
-	}
+	routes.AddRoutes(app)
 
 	// run the app
 	app.Listen(":5000")
---
+++ backend/routes/routes.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	"backend/routes/oauth"
+	"backend/routes/tournament"
+	"backend/routes/user"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AddRoutes(app *fiber.App) {
+	usersGroup := app.Group("/users")
+	{
+		usersGroup.Get("/", user.List)
+		usersGroup.Get("/self", user.GetSelf)
+		usersGroup.Get("/:id", user.Get)
+	}
+
+	oauthGroup := app.Group("/oauth")
+	{
+		oauthGroup.Get("/login", oauth.Login)
+	}
+
+	tournamentGroup := app.Group("/tournaments")
+	{
+		tournamentGroup.Get("/", tournament.List)
+		tournamentGroup.Get("/:id", tournament.Get)
+		tournamentGroup.Post("/", tournament.Create)
+		tournamentGroup.Put("/:id", tournament.Update)
+		tournamentGroup.Delete("/:id", tournament.Delete)
+	}
+
+	mappoolerGroup := app.Group("/mappoolers")
+	{
+		mappoolerGroup.Post("/", tournament.AddMappooler)
+		mappoolerGroup.Delete("/", tournament.RemoveMappooler)
+	}
+
+	testplayerGroup := app.Group("/testplayers")
+	{
+		testplayerGroup.Post("/", tournament.AddTestplayer)
+		testplayerGroup.Delete("/", tournament.RemoveTestplayer)
+	}
+}
