3282adf0ed53f7a79cb66c78d7092d47d0e1c757
Maximilian Kruse <maxkruse1996@gmail.com>
2022-10-01 21:49:31 +0200

    use osuapi client to fill map stats

--- backend/services/osuapiservice/service.go
+++ backend/services/osuapiservice/service.go
@@ -3,24 +3,25 @@ package osuapiservice
 import (
 	"backend/services/userservice"
 	"context"
-	"net/http"
+
+	gosuapiclient "github.com/maxkruse/gosu-api-client"
 
 	"golang.org/x/oauth2"
 )
 
-func NewClient(auth_token string) (*http.Client, error) {
+func NewClient(auth_token string) (*gosuapiclient.Client, error) {
 	// get self from auth token
 	self, err := userservice.GetUserFromToken(auth_token)
 	if err != nil {
 		return nil, err
 	}
 
-	oauthClient := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(&oauth2.Token{
+	oauthClient := gosuapiclient.NewClient(oauth2.Token{
 		AccessToken:  self.Token.AccessToken,
 		TokenType:    self.Token.TokenType,
 		RefreshToken: self.Token.RefreshToken,
 		Expiry:       self.Token.Expiry,
-	}))
+	}, context.Background())
 
 	return oauthClient, nil
 }
